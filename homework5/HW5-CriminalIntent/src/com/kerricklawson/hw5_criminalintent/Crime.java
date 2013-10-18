262eee75b4896ca0ea4a7e780186e9d73faa9807
KerrickLawson <kerricklawson@gmail.com>
2013-10-18 17:47:50 -0400

    HW5-Chapter 8
    Updated CriminalIntent

--- homework5/HW5-CriminalIntent/src/com/kerricklawson/hw5_criminalintent/Crime.java
+++ homework5/HW5-CriminalIntent/src/com/kerricklawson/hw5_criminalintent/Crime.java
@@ -1,13 +1,16 @@
 package com.kerricklawson.hw5_criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
 	
     private UUID mId;
-    
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+    
     
     public Crime() {
         mId = UUID.randomUUID();
@@ -24,5 +27,21 @@ public class Crime {
     public UUID getId() {
         return mId;
     }
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
+	}
 	
 }
--- homework5/HW5-CriminalIntent/src/com/kerricklawson/hw5_criminalintent/CrimeFragment.java
+++ homework5/HW5-CriminalIntent/src/com/kerricklawson/hw5_criminalintent/CrimeFragment.java
@@ -1,7 +1,5 @@
 package com.kerricklawson.hw5_criminalintent;
 
-import com.kerricklawson.criminalintent.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -9,11 +7,19 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+
+import com.kerricklawson.criminalintent.R;
 
 public class CrimeFragment extends Fragment {
 	private Crime mCrime;
 	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
 	
 	
 	@Override
@@ -38,6 +44,18 @@ public class CrimeFragment extends Fragment {
 	public void afterTextChanged(Editable c) {
 	// This one too
 	}
+	});
+    
+    mDateButton = (Button)v.findViewById(R.id.crime_date);
+    mDateButton.setText(mCrime.getDate().toString());
+    mDateButton.setEnabled(false);
+    
+    mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+    mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            // set the crime's solved property
+            mCrime.setSolved(isChecked);
+        }
 	});
 	return v;
 	}
