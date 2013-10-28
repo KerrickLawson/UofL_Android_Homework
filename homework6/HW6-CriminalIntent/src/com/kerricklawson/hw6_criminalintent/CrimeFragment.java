21363bedee55b8f5dc39701e03b4393216c9829a
KerrickLawson <kerricklawson@gmail.com>
2013-10-28 16:46:06 -0400

    HW6-Chapter 10
    Updated Criminal Intent to chapter 10 material

--- homework6/HW6-CriminalIntent/src/com/kerricklawson/hw6_criminalintent/CrimeFragment.java
+++ homework6/HW6-CriminalIntent/src/com/kerricklawson/hw6_criminalintent/CrimeFragment.java
@@ -1,5 +1,7 @@
 package com.kerricklawson.hw6_criminalintent;
 
+import java.util.UUID;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -16,48 +18,67 @@ import android.widget.EditText;
 import com.kerricklawson.criminalintent.R;
 
 public class CrimeFragment extends Fragment {
+
+	public static final String EXTRA_CRIME_ID = "criminalintent.CRIME_ID";
+
 	private Crime mCrime;
 	private EditText mTitleField;
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;
-	
-	
+
+	public static CrimeFragment newInstance(UUID crimeId) {
+		Bundle args = new Bundle();
+		args.putSerializable(EXTRA_CRIME_ID, crimeId);
+
+		CrimeFragment fragment = new CrimeFragment();
+		fragment.setArguments(args);
+
+		return fragment;
+	}
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	mCrime = new Crime();
+		super.onCreate(savedInstanceState);
+        UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
-	Bundle savedInstanceState) {
-	View v = inflater.inflate(R.layout.fragment_crime, parent, false);
-	
-    mTitleField = (EditText)v.findViewById(R.id.crime_title);
-    mTitleField.addTextChangedListener(new TextWatcher() {
-        public void onTextChanged(CharSequence c, int start, int before, int count) {
-            mCrime.setTitle(c.toString());
-	}
-	public void beforeTextChanged(
-	CharSequence c, int start, int count, int after){
-	}
-	public void afterTextChanged(Editable c) {
-	// This one too
-	}
-	});
-    
-    mDateButton = (Button)v.findViewById(R.id.crime_date);
-    mDateButton.setText(mCrime.getDate().toString());
-    mDateButton.setEnabled(false);
-    
-    mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
-    mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            // set the crime's solved property
-            mCrime.setSolved(isChecked);
-        }
-	});
-	return v;
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+		mTitleField = (EditText) v.findViewById(R.id.crime_title);
+		mTitleField.setText(mCrime.getTitle());
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			public void onTextChanged(CharSequence c, int start, int before,
+					int count) {
+				mCrime.setTitle(c.toString());
+			}
+
+			public void beforeTextChanged(CharSequence c, int start, int count,
+					int after) {
+			}
+
+			public void afterTextChanged(Editable c) {
+				// This one too
+			}
+		});
+
+		mDateButton = (Button) v.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+
+		mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox
+				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
+						// set the crime's solved property
+						mCrime.setSolved(isChecked);
+					}
+				});
+		return v;
 	}
 
 }
